412c7cee550603e5d4341e47a460d4a5e4fc1930
Michele Cavaioni <41079648+Udacavs@users.noreply.github.com>
2019-09-25 13:57:28 -0400

    added capstone starter code

---
+++ projects/capstone/heroku_sample/starter/app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from models import setup_db
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['EXCITED']
+        greeting = "Hello" 
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
