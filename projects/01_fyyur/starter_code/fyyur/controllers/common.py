b1a0cdfd4258b834a472ad6ebac14614f921b63e
innocent lou <bisonlou@gmail.com>
2019-11-14 23:49:25 +0300

    added latest venues

--- projects/01_fyyur/starter_code/fyyur/controllers/common.py
+++ projects/01_fyyur/starter_code/fyyur/controllers/common.py
@@ -1,12 +1,14 @@
 from fyyur import app, db
 from fyyur.models.artist import Artist
+from fyyur.models.venue import Venue
 from flask import render_template
 
 
 @app.route('/')
 def index():
     artists = Artist.query.order_by(Artist.id.desc()).limit(10)
-    return render_template('pages/home.html', artists=artists)
+    venues = Venue.query.order_by(Venue.id.desc()).limit(10)
+    return render_template('pages/home.html', artists=artists, venues=venues)
 
 @app.errorhandler(404)
 def not_found_error(error):
