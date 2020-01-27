8c4b7dcbfb9521b274dd01b2d29c72aae4fbfd84
Filipe Bezerra <filipebzerra@gmail.com>
2020-01-27 04:04:58 -0300

    Database backup

---
+++ projects/01_fyyur/solution/backup/artist_202001270404.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.artist (name,city,state,phone,genres,image_link,facebook_link,website,seeking_venue,seeking_description) VALUES 
+('Guns N Petals','San Francisco','CA','326-123-5000','Rock n Roll','https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80','https://www.facebook.com/GunsNPetals','https://www.gunsnpetalsband.com',true,'Looking for shows to perform at in the San Francisco Bay Area!')
+,('Matt Quevedo','New York','NY','300-400-5000','Jazz','https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80','https://www.facebook.com/mattquevedo923251523',NULL,false,NULL)
+,('The Wild Sax Band','San Francisco','CA','432-325-5432','Jazz, Classical','https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',NULL,NULL,false,NULL)
+;
