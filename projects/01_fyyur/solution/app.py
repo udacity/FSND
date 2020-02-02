b92d755b653f006da97dc5da950099636b748f6f
Filipe Bezerra <filipebzerra@gmail.com>
2020-02-01 22:21:51 -0300

    Update models with nullable constraint and defaults

--- projects/01_fyyur/solution/app.py
+++ projects/01_fyyur/solution/app.py
@@ -36,11 +36,11 @@ class Venue(db.Model):
     __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    genres = db.Column(db.String(120))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
@@ -110,11 +110,11 @@ class Artist(db.Model):
     __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
@@ -184,7 +184,7 @@ class Show(db.Model):
   artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
   venue = db.relationship('Venue', backref='shows', lazy=True)
   artist = db.relationship('Artist', backref='shows', lazy=True)
-  start_time = db.Column(db.DateTime, nullable=False)
+  start_time = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
 
   def format(self):
     return {
