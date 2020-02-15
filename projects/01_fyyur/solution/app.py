09a22f4bc0735e89ba5db2233b69fc3740502983
Filipe Bezerra <filipe.sousa@softplan.com.br>
2020-02-15 03:17:02 -0300

    Rollbacks any database changes

--- projects/01_fyyur/solution/app.py
+++ projects/01_fyyur/solution/app.py
@@ -514,11 +514,12 @@ def create_show_submission():
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('errors/404.html'), 404
+  return render_template('errors/404.html'), 404
 
 @app.errorhandler(500)
 def server_error(error):
-    return render_template('errors/500.html'), 500
+  db.session.rollback()
+  return render_template('errors/500.html'), 500
 
 
 if not app.debug:
