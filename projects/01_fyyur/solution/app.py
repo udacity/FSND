6fa0b09a62aad1b2eb6d95a6f15b69c917ebb81c
Filipe Bezerra <filipe.sousa@softplan.com.br>
2020-02-15 20:31:52 -0300

    Implements artist search endpoint and fixes venue search form

--- projects/01_fyyur/solution/app.py
+++ projects/01_fyyur/solution/app.py
@@ -394,15 +394,19 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  search_term = request.form.get('search_term', '')
+
+  artists_found = Artist.query.filter(Artist.name.match(f'%{search_term}%')).all()
+
+  formatted_artists = [{
+    'id': artist.id,
+    'name': artist.name,
+    'num_upcoming_shows': artist.upcoming_shows_count
   }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  for artist in artists_found]
+  response = {'count': len(artists_found), 'data': list(formatted_artists)}
+
+  return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
