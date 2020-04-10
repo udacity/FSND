b1cbb48ac92f22a3d3f680986bceff6bef85034f
Chad <eliasargandara@gmail.com>
2020-04-09 21:37:39 -0700

    configured database connection and migration scripts

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
 
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# Turn off SQLAlchemy modification tracking to avoid deprecation warning and overhead
+SQLALCHEMY_TRACK_MODIFICATIONS = False
