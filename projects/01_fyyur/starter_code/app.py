1a09a95fe907cdc93848324c829b84af53a76198
Manuela Mercado <mmercado@intellisys.com.do>
2020-04-11 18:17:29 -0400

    Adding fields to artist

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -48,13 +48,16 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website= db.Column(db.String(120))
+    seeking_venue= db.Column(db.Boolean(), nullable=False, default=False)
+    seeking_description= db.Column(db.String())
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
