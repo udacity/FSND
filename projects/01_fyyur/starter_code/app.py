e6d44e616d872eedfb6aef54275d3f1224a1fd42
Chad <eliasargandara@gmail.com>
2020-04-12 16:42:38 -0700

    updated database schema

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
@@ -27,37 +28,129 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
+venue_genres = db.Table(
+    'Venue_Genres',
+    db.Column(
+        'venue_id',
+        db.Integer,
+        db.ForeignKey('Venue.id', ondelete='CASCADE'),
+        primary_key=True),
+    db.Column(
+        'genre_id',
+        db.Integer,
+        db.ForeignKey('Genre.id', ondelete='CASCADE'),
+        primary_key=True))
+
+artist_genres = db.Table(
+    'Artist_Genres',
+    db.Column(
+        'artist_id',
+        db.Integer,
+        db.ForeignKey('Artist.id', ondelete='CASCADE'),
+        primary_key=True),
+    db.Column(
+        'genre_id',
+        db.Integer,
+        db.ForeignKey('Genre.id', ondelete='CASCADE'),
+        primary_key=True))
+
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
+    name = db.Column(db.String(), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String)
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(
+        db.String,
+        nullable=False,
+        default='We are looking for local artists to perform at our shows!')
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    shows = db.relationship(
+        'Show',
+        backref='venue',
+        passive_deletes=True)
+
+    genres = db.relationship(
+        'Genre',
+        secondary=venue_genres,
+        backref='venue',
+        passive_deletes=True)
+
+    UniqueConstraint('name', 'city', 'state', 'address')
+
+    def __repr__(self):
+        return f'<Venue id: {self.id}, name: {self.name}>'
 
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String)
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(
+        db.String,
+        nullable=False,
+        default='Looking for shows to perform at!')
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    shows = db.relationship(
+        'Show',
+        backref='artist',
+        passive_deletes=True)
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+    genres = db.relationship(
+        'Genre',
+        secondary=artist_genres,
+        backref='artist',
+        passive_deletes=True)
+
+    def __repr__(self):
+        return f'<Artist id: {self.id}, name: {self.name}>'
+
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.TIMESTAMP(timezone=True), nullable=False)
+
+    venue_id = db.Column(
+        db.Integer,
+        db.ForeignKey('Venue.id', ondelete='CASCADE'),
+        nullable=False)
+
+    artist_id = db.Column(
+        db.Integer,
+        db.ForeignKey('Artist.id', ondelete='CASCADE'),
+        nullable=False)
+
+    def __repr__(self):
+        return f'<Show id: {self.id}, venue id: {self.venue_id}, artist id: {self.artist_id}>'
+
+
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<Genre id: {self.id}, name: {self.name}>'
+
 
 #----------------------------------------------------------------------------#
 # Filters.
