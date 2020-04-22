ec9834e7770c0b165f1ec76e1c7e1e69173a325b
p6l-richard <richard.poelder@gmail.com>
2020-04-22 07:30:34 +0200

    Add `Defining columns` [Set up Models]

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -38,8 +38,13 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.PickleType)
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # OK! 
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -52,8 +57,24 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # OK!
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    # artist_id = db.Column(db.Integer, db.ForeignKey('artist_show.artist_id'))
+    # venue_id = db.Column(db.Integer, db.ForeignKey('venue_show.venue.id'))
+    start_time = db.Column(db.DateTime, nullable=False)
+
+
+
+
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
