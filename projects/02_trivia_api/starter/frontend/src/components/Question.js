cfbf6d0ffa6d53c29413f93c886f6f97f0233090
Shir Bar Lev <anncarters@Shirs-MacBook-Air-2.local>
2020-04-23 19:37:18 +0300

    Added 'alt' attribute to images.

--- projects/02_trivia_api/starter/frontend/src/components/Question.js
+++ projects/02_trivia_api/starter/frontend/src/components/Question.js
@@ -19,9 +19,9 @@ class Question extends Component {
       <div className="Question-holder">
         <div className="Question">{question}</div>
         <div className="Question-status">
-          <img className="category" src={`${category}.svg`}/>
+          <img className="category" alt="category" src={`${category}.svg`}/>
           <div className="difficulty">Difficulty: {difficulty}</div>
-          <img src="delete.png" className="delete" onClick={() => this.props.questionAction('DELETE')}/>
+          <img src="delete.png" alt="delete question" className="delete" onClick={() => this.props.questionAction('DELETE')}/>
           
         </div>
         <div className="show-answer button"
