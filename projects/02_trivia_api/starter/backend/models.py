db5614358ec86c0cb8ecc56bb6838c0f76cb1aa5
Eliasar Gandara <eliasargandara@gmail.com>
2020-04-24 20:49:47 -0700

    added env variable loading for database connection

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -1,10 +1,25 @@
 import os
+import sys
+from dotenv import load_dotenv
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+load_dotenv()
+
+database_user = os.getenv("DB_USER", "trivia_app")
+database_password = os.getenv("DB_PASSWORD")
+database_host = os.getenv("DB_HOST", "localhost")
+database_port = os.getenv("DB_PORT", "5432")
+database_name = os.getenv("DB_NAME", "trivia")
+
+if not database_password:
+    print('Please provide a non-empty database password using the environment variable "DB_PASSWORD"')
+    sys.exit(1)
+
+database_path = "postgresql://{}:{}@{}:{}/{}".\
+    format(database_user, database_password,
+           database_host, database_port, database_name)
 
 db = SQLAlchemy()
 
@@ -12,6 +27,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -19,60 +36,66 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+
 '''
 Question
 
 '''
-class Question(db.Model):  
-  __tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+class Question(db.Model):
+    __tablename__ = 'questions'
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'category': self.category,
+            'difficulty': self.difficulty
+        }
+
 
 '''
 Category
 
 '''
-class Category(db.Model):  
-  __tablename__ = 'categories'
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
 
-  def __init__(self, type):
-    self.type = type
+class Category(db.Model):
+    __tablename__ = 'categories'
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
+
+    def __init__(self, type):
+        self.type = type
+
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
