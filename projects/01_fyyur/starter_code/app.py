4cdfc30a480cda31d01c96b2c154f435b6912f34
p6l-richard <richard.poelder@gmail.com>
2020-04-29 09:43:03 +0200

    Add update artist

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -501,28 +501,46 @@ def edit_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
+  data = {key:val for key,val in request.form.items() if key != 'genres'}
+  data.update({'genres':request.form.getlist('genres')})
 
-  return redirect(url_for('show_artist', artist_id=artist_id))
+  try:
+    artist = db.session.query(Artist).get(artist_id)
+    for key, val in data.items():
+      setattr(artist, key, val)
+    db.session.commit()
+    print(artist.as_dict())
+    return redirect(url_for('show_artist', artist_id=artist_id))
+  except:
+    db.session.rollback()
+    print('something went wrong', traceback.format_exc())
+    return 'error:' + traceback.format_exc(), 200
+  finally:
+    db.session.close()  
+
+  # TODO: take values from the form submitted, and update existing
+  # artist record with ID <artist_id> using the new attributes 
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
+  # {
+  #   "id": 1,
+  #   "name": "The Musical Hop",
+  #   "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+  #   "address": "1015 Folsom Street",
+  #   "city": "San Francisco",
+  #   "state": "CA",
+  #   "phone": "123-123-1234",
+  #   "website": "https://www.themusicalhop.com",
+  #   "facebook_link": "https://www.facebook.com/TheMusicalHop",
+  #   "seeking_talent": True,
+  #   "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+  #   "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+  # }
+  venue=db.session.query(Venue).get(venue_id).as_dict()
+  print(venue)
+  ven_li = {key:value_formatted(key, val) for key,val in venue.items()}
+  form = VenueForm(**ven_li)
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
@@ -659,6 +677,7 @@ def server_error(error):
 
 def value_formatted(key, value):
     if key == 'genres':
+      print(value)
       value = value.replace('{','')
       value = value.replace('}','')
       value = value.split(',')
