f1997e1d0785ee77beecaad0395f4a88d57d0c39
derekyesmunt <qSSA534jGZ32dAb>
2020-04-29 18:03:41 -0400

    feat: created [GET] endpoint for '/actors'
    -Database query for all actors
    -iterate over actors to format and create list of formatted actors
    -created jsonify output with status code, success, num of actors and actor list

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import setup_db
+from models import setup_db, Actor, Movie
 
 
 def create_app(test_config=None):
@@ -13,10 +13,24 @@ def create_app(test_config=None):
 
     @app.route('/')
     def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello"
-        if excited == 'true': greeting = greeting + "!!!!!"
+        greeting = 'it works!'
         return greeting
+
+
+    @app.route('/actors')
+    def get_actors():
+        actor_list = []
+        actors = Actor.query.all()
+
+        for actor in actors:
+            actor_list.append(actor.format())
+
+        return jsonify({
+            'status_code': 200,
+            'success': True,
+            'num_of_actors': len(actor_list),
+            'actors_list': actor_list
+        })
 
     return app
 
