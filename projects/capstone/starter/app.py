8dd6a112b6d9dc5f6f3122962ece56fd9794df5a
derekyesmunt <qSSA534jGZ32dAb>
2020-04-28 21:35:20 -0400

    Initial Capstone Progject commit
    Created models.py and populated with complete models for Movies and Actors with appropriate columns.

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,15 +2,26 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['EXCITED']
+        greeting = "Hello"
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
 
-APP = create_app()
+    return app
+
+
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,77 @@
+import os
 
+from sqlalchemy import Column, String, Integer, Date, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = os.environ.get('DATABASE_URL')
+if not database_path:
+    database_name = "agency"
+    database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app) - binds the flask application and SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Movies
+have title and release date
+'''
+
+
+class Movie(db.Model):
+    __tablename__ = 'Movies'
+
+    id = db.Column(Integer, primary_key=True)
+    title = db.Column(String, )
+    release_date = db.Column(Date)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+
+'''
+Actors
+have name, age, and gender
+'''
+
+
+class Actor(db.Model):
+    __tablename__ = 'Actors'
+
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String)
+    age = db.Column(Integer)
+    gender = db.Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name,
+        self.age = age,
+        self.gender = gender
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
