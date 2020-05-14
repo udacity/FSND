cf45df5d15bdf6c3f9dfab6a32d9eb6c0ddac4cd
p6l-richard <richard.poelder@gmail.com>
2020-05-14 10:50:23 +0200

    Refactor to hard code db password

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -16,7 +16,8 @@ def create_db_path(**kwargs):
 
   default_or_testing = 'default' if database_name == 'trivia' else 'testing'
   ask_for_pw = f"Insert password for {default_or_testing} database user: "
-  password = input(ask_for_pw)
+  user_pw = input(ask_for_pw)
+  password = '234107' if user_pw is not None else user_pw
   return f"postgresql://{db_user}:{password}@localhost:5432/{database_name}"
 
 database_path = create_db_path()
