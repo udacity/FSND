b597a9d16558d420529b76d4531d228d2d2d2b48
p6l-richard <richard.poelder@gmail.com>
2020-05-14 12:18:49 +0200

    Add endpoint for <questions/categories/cat_id>

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -101,6 +101,21 @@ def create_app(test_config=None):
   def get_all_questions():
     try:
       result = Question.query.order_by(Question.id).all()
+      if len(result) < 1:
+        return 'Resource not found', 404
+      paginated_questions = paginate_result(result) 
+      return jsonify(
+          format_response(paginated_questions, result)
+        )
+    except:
+      print(traceback.print_exc())
+      return 'ERROR:' + str(traceback.print_exc()), 400
+  @app.route('/api/questions/categories/<int:category_id>')
+  def get_all_questions_by_category(category_id):
+    try:
+      result = Question.query.filter_by(category_id=category_id).order_by(Question.id).all()
+      if len(result) < 1:
+        return 'Resource not found', 404
       paginated_questions = paginate_result(result) 
       return jsonify(
           format_response(paginated_questions, result)
