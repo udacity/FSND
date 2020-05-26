8eb103a200b04983b075bd9d6f19989de058e356
Benedek Tamas <benedek.thomas@gmail.com>
2020-05-26 14:56:42 +0200

    added DB URI

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://benedekthomas@localhost:5432/fyuur_app'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
