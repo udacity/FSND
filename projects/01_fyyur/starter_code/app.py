93e86d5f94945330123b2cc7b5b532fe39fe5954
Lisa Koss <lisakoss@uw.edu>
2020-05-29 01:17:23 -0700

    Add search for venues

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -152,16 +152,24 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+  # query db for venues using search term
+  # returns all matching results; case insensitive
+  search_term = request.form.get('search_term')
+  venues = Venue.query.filter(Venue.name.ilike(f'%{search_term}%')).all()
+
+  data = []
+
+  for venue in venues:
+    venueInfo = {
+      "id": venue.id,
+      "name": venue.name,
+    }
+
+    data.append(venueInfo)
+
+  response = {
+    "count": len(venues),
+    "data": data
   }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
