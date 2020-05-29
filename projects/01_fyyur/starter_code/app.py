0afe746cc6f8a20f79aa1d593da6605b41b79c28
Lisa Koss <lisakoss@uw.edu>
2020-05-29 01:19:29 -0700

    Add search for artists

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -171,6 +171,7 @@ def search_venues():
     "count": len(venues),
     "data": data
   }
+
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
@@ -294,17 +295,26 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  # query db for artists using search term
+  # returns all matching results; case insensitive
+  search_term = request.form.get('search_term')
+  artists = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).all()
+
+  data = []
+
+  for artist in artists:
+    artistInfo = {
+      "id": artist.id,
+      "name": artist.name,
+    }
+
+    data.append(artistInfo)
+
+  response = {
+    "count": len(artists),
+    "data": data
   }
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
