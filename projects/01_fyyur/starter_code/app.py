310942edae14274dd7c82c1970697b362ec49166
Lisa Koss <lisakoss@uw.edu>
2020-05-29 02:41:28 -0700

    Populate artist edit form

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -379,21 +379,24 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
+  artist = Artist.query.filter_by(id=artist_id).first()
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
+
+  # must process first before form is loaded
+  form.genres.default = artist.genres
+  form.state.default = artist.state
+  form.seeking_venue.default = 'Yes' if artist.seeking_venue == True else 'No'
+  form.process()
+
+  # populate rest of form
+  form.name.data = artist.name
+  form.city.data = artist.city
+  form.phone.data = artist.phone
+  form.facebook_link.data = artist.facebook_link
+  form.website_link.data = artist.website_link
+  form.image_link.data = artist.image_link
+  form.seeking_desc.data = artist.seeking_desc
+
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
