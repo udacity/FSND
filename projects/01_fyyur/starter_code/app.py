7ba158c7b2dca458b8a62c331af042cf19ff5d92
Alexis Koss <alexis30@uw.edu>
2020-05-29 01:51:56 -0700

    add real data to artists page from db

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -117,34 +117,9 @@ def index():
 
 @app.route('/venues')
 def venues():
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-
-  # list of dictionaries of unique city/state -> list of dictionary venues
-
   # list to store data from the Venues table
   data = [] 
+
   city_state = {}
   venues = []
   previous_state = ''
@@ -339,17 +314,17 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data = []
+  ordered_artists = Artist.query.order_by(Artist.name).all()
+
+  for artist in ordered_artists:
+    artist_to_add = {
+      "id": artist.id,
+      "name": artist.name
+    }
+
+    data.append(artist_to_add)
+
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
@@ -365,6 +340,7 @@ def search_artists():
       "num_upcoming_shows": 0,
     }]
   }
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
