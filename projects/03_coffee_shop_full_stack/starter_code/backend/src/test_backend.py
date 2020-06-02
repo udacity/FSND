a2facb0d6efd0aee463f7dd14ba0cb7df42d7230
p6l-richard <richard.poelder@gmail.com>
2020-06-02 11:07:05 +0200

    Implement tests (failing)

---
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/test_backend.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+
+import pytest
+
+from src import app
+from .database import setup_db
+
+
+@pytest.fixture
+def client():
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        with app.app_context():
+            setup_db(app)
+        yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
+
+
+def test_get_homepage(client):
+    """Start with the hello string."""
+
+    rv = client.get('/')
+    assert b'hello' in rv.data
+    assert 200 == rv.status_code
+
+
+def test_404_get_random_route(client):
+    """"Get route not yet implemented"""
+    rv = client.get('/not-yet-implemented')
+    assert b'not found' in rv.data.lower()
+    assert 404 == rv.status_code
