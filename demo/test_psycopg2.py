76d568f7698622a0a42745bbe78dec474bfa6c87
Francis Sziebert <franksziebert@gmail.com>
2020-06-01 19:55:50 -0600

    adds demo code from "basic usage"

---
+++ demo/test_psycopg2.py
@@ -0,0 +1,35 @@
+import os
+import psycopg2
+
+password_path = os.path.dirname(os.path.abspath(__file__)) + '/../../.dbpassword'
+print('password_path: {}'.format(password_path))
+
+with open(password_path) as fp:
+	password = fp.read()
+
+print("password: {}".format(password))
+
+conn = psycopg2.connect('dbname=test_psycopg2 user=frank password={}'.format(password))
+
+cursor = conn.cursor()
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# drop any existing todos table
+cur.execute("DROP TABLE IF EXISTS todos;")
+
+# (re)create the todos table
+# (note: triple quotes allow multiline text in python)
+cur.execute("""
+  CREATE TABLE todos (
+    id serial PRIMARY KEY,
+    description VARCHAR NOT NULL
+  );
+""")
+
+# commit, so it does the executions on the db and persists in the db
+conn.commit()
+
+cur.close()
+conn.close()
