30c7e43af69b612fb49914756b2f4869249dc382
Francis Sziebert <franksziebert@gmail.com>
2020-06-02 19:41:42 -0600

    sqlalchemy basics exercise 3

---
+++ demo/test_sqlalchemy/app.py
@@ -0,0 +1,32 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+# Get password from outside of repo
+
+password_path = os.path.dirname(os.path.abspath(__file__)) + '/../../../.dbpassword'
+print('password_path: {}'.format(password_path))
+with open(password_path) as fp:
+    password = fp.read().strip()
+print("password: {}".format(password))
+
+# Lesson Exercises
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://frank:{}@localhost:5432/test_sqlalchemy'.format(password)
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
+db.create_all()
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run()
