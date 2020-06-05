dcf04ffae2d636f82ddddcebe2a130d9b7563162
Francis Sziebert <franksziebert@gmail.com>
2020-06-04 22:04:07 -0600

    adds SQL statements

--- demo/todo_app/app.py
+++ demo/todo_app/app.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+# Get password from outside of repo  
+password_path = os.path.dirname(os.path.abspath(__file__)) + '/../../../.dbpassword' 
+print('password_path: {}'.format(password_path)) 
+with open(password_path) as fp:
+    password = fp.read().strip() 
+print("password: {}".format(password))
+
+
+# application initialization
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://frank:{}@localhost:5432/lesson5'.format(password) 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+# model definition
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}'
+
+db.create_all()
+
+# routes
 @app.route('/')
 def index():
     data = [
