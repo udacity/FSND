1ce238d745a1eb8b7e94390f0a4f9155c0d87f92
Francis Sziebert <franksziebert@gmail.com>
2020-06-04 21:32:53 -0600

    5.2 jinja loops

--- demo/todo_app/app.py
+++ demo/todo_app/app.py
@@ -1,7 +1,16 @@
-from flask import Flask render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    data = [
+            {'description': 'Todo 1'},
+            {'description': 'Todo 2'},
+            {'description': 'Todo 3'},
+            {'description': 'Todo 4'}
+            ]
     return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run()
