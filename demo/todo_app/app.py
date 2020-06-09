e697c1b32bf18f1584a8aa78e01bb1c064b24567
Francis Sziebert <franksziebert@gmail.com>
2020-06-08 21:33:07 -0600

    added new completed column to Todo

--- demo/todo_app/app.py
+++ demo/todo_app/app.py
@@ -28,6 +28,7 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}'
