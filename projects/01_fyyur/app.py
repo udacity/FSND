4d3ea14aff2317bb6d15517bb0476e46198a4107
Francis Sziebert <fsziebert@cradlepoint.com>
2020-06-19 16:32:49 -0600

    show and edit pages updated

--- projects/01_fyyur/app.py
+++ projects/01_fyyur/app.py
@@ -450,8 +450,6 @@ def search_artists():
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
-    # shows the venue page with the given venue_id
-    # TODO: replace with real venue data from the venues table, using venue_id
     artist = Artist.query.get(artist_id)
     if isinstance(artist, Artist):
         data = artist.serialize
@@ -467,22 +465,15 @@ def show_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
+    artist = Artist.query.get(artist_id)
     form = ArtistForm()
-    artist={
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-    }
-    # TODO: populate form with fields from artist with ID <artist_id>
-    return render_template('forms/edit_artist.html', form=form, artist=artist)
+    if isinstance(artist, Artist):
+        data = artist.serialize
+        print(json.dumps(data, indent=4))
+        return render_template('forms/edit_artist.html', form=form, artist=data)
+    else:
+        return abort(404)
+
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
