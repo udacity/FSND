d4242b39346d12da60343313ec3d604cab8722d5
Nicolas Hu <46500945+NicolasHu11@users.noreply.github.com>
2020-07-06 17:14:28 -0400

    connected local postgresql database

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@localhost:5432/fyyur_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False 
