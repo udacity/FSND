aa0c6ba88681fa153cc055c06863ffb0f63ae0f3
Francis Sziebert <franksziebert@gmail.com>
2020-07-07 13:36:21 -0600

    fixes setup_db call in backend/test_flaskr.py

--- projects/02_trivia_api/Francis_Sziebert_02_Trivia_API/backend/test_flaskr.py
+++ projects/02_trivia_api/Francis_Sziebert_02_Trivia_API/backend/test_flaskr.py
@@ -15,8 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+        setup_db(self.app, self.database_name)
 
         # binds the app to the current context
         with self.app.app_context():
@@ -28,6 +27,7 @@ class TriviaTestCase(unittest.TestCase):
     def tearDown(self):
         """Executed after reach test"""
         pass
+
 
 
     """
