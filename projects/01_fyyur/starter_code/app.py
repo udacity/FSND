9b0ef96571abac0091750ff7ae81c52bf9a990e1
Nicolas Hu <46500945+NicolasHu11@users.noreply.github.com>
2020-07-11 00:23:55 -0400

    search for artist implemented

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -392,17 +392,7 @@ def delete_venue(venue_id):
 #----------------------------------------------------------------------------#
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-    }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-    }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-    }]
+    """display a list of artists """
     
     data = Artist.query.all()
     result = [] 
@@ -411,22 +401,28 @@ def artists():
             'id': a.id,
             'name': a.name
             })
-    
     return render_template('pages/artists.html', artists=result)
 
+
 @app.route('/artists/search', methods=['POST'])
-def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
-    response={
-    "count": 1,
-    "data": [{
-        "id": 4,
-        "name": "Guns N Petals",
-        "num_upcoming_shows": 0,
-    }]
-    }
+def search_artists():        
+    target = request.form['search_term'].lower()
+    search = f"%{target}%"
+    data = Artist.query.filter(Artist.name.ilike(search)).all() # ilike is case insensitive
+    
+    result = []
+    for a in data:
+        logging.info(a.name)
+        result.append({
+            'id': a.id,
+            'name': a.name,
+            'num_upcoming_shows': 'not implemented'
+            })
+    response = {
+        'count': len(result),
+        'data': result
+        }
+    # return jsonify(response)
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
