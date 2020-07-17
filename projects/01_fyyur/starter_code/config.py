c527383375735439a345e02654ebbb3f51345c1d
iazer <ibrahim.samir.ayad@hotmail.com>
2020-07-17 19:53:26 +0300

    config database and project environment

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -5,9 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
-
+ENV = 'development'
 # Connect to the database
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost:5432/Fyyurapp'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
