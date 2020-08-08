bd0f5d76926bf1cc599064ccc5e823dcf29a69c6
Francis Sziebert <franksziebert@gmail.com>
2020-08-07 20:43:23 -0600

    fixed issue due to https callback

--- projects/03_coffee_shop_full_stack/03_Francis_Sziebert_Coffee_Shop_Full_Stack/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/03_Francis_Sziebert_Coffee_Shop_Full_Stack/frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     url: 'flis.us', // the auth0 domain prefix
     audience: 'coffee', // the audience set for the auth0 app
     clientId: 'I12UnrxbhRGHcQG9wCyJwARE3YN81WLz', // the client id generated for the auth0 app
-    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application.
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application.
   }
 };
