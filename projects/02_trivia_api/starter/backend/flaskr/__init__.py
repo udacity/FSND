1c362584ad748a8aa86efb4f123e4ab9f8ffe8b4
clay-coffman <claymcoffman@gmail.com>
2020-08-24 13:02:13 -0700

    fix werkzeug ver, init api, cors, GET categories

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -8,27 +8,49 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  
-  '''
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+
+    """
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+  """
 
-  '''
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    """
   @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  """
 
-  '''
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization"
+        )
+        response.headers.add("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS")
+
+        return response
+
+    """
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
-  '''
+  """
 
+    @app.route("/api/categories")
+    def get_categories():
+        # get all categories
 
-  '''
+        categories = Category.query.all()
+        result_categories = []
+        for category in categories:
+            formatted_category = category.format()
+            result_categories.append(formatted_category)
+        return jsonify(result_categories)
+
+    """
   @TODO: 
   Create an endpoint to handle GET requests for questions, 
   including pagination (every 10 questions). 
@@ -39,17 +61,17 @@ def create_app(test_config=None):
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
-  '''
+  """
 
-  '''
+    """
   @TODO: 
   Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
-  '''
+  """
 
-  '''
+    """
   @TODO: 
   Create an endpoint to POST a new question, 
   which will require the question and answer text, 
@@ -58,9 +80,9 @@ def create_app(test_config=None):
   TEST: When you submit a question on the "Add" tab, 
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
-  '''
+  """
 
-  '''
+    """
   @TODO: 
   Create a POST endpoint to get questions based on a search term. 
   It should return any questions for whom the search term 
@@ -69,19 +91,18 @@ def create_app(test_config=None):
   TEST: Search by any phrase. The questions list will update to include 
   only question that include that string within their question. 
   Try using the word "title" to start. 
-  '''
+  """
 
-  '''
+    """
   @TODO: 
   Create a GET endpoint to get questions based on category. 
 
   TEST: In the "List" tab / main screen, clicking on one of the 
   categories in the left column will cause only questions of that 
   category to be shown. 
-  '''
+  """
 
-
-  '''
+    """
   @TODO: 
   Create a POST endpoint to get questions to play the quiz. 
   This endpoint should take category and previous question parameters 
@@ -91,14 +112,14 @@ def create_app(test_config=None):
   TEST: In the "Play" tab, after a user selects "All" or a category,
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
-  '''
+  """
 
-  '''
+    """
   @TODO: 
   Create error handlers for all expected errors 
   including 404 and 422. 
-  '''
-  
-  return app
+  """
 
-    
+    return app
+
+
