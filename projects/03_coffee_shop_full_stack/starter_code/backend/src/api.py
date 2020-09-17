5c411f9c1cd13548f86bba57eef15b8b19798662
clay-coffman <claymcoffman@gmail.com>
2020-09-16 17:01:51 -0700

    upgrade werkzeug

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -11,35 +11,46 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
+"""
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-'''
-# db_drop_and_create_all()
+"""
+db_drop_and_create_all()
 
 ## ROUTES
-'''
+"""
 @TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+@app.route("/api/drinks")
+def get_drinks():
+    drinks = Drink.query.all()
+
+    formatted_drinks = [drink.format() for drink in drinks]
+
+    res = make_response(jsonify({"drinks": formatted_drinks}), 200)
+
+    return res
+
+
+"""
 @TODO implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+"""
 @TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
@@ -47,10 +58,10 @@ CORS(app)
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+"""
 @TODO implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
@@ -60,10 +71,10 @@ CORS(app)
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+"""
 @TODO implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
@@ -72,22 +83,24 @@ CORS(app)
         it should require the 'delete:drinks' permission
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
 ## Error Handling
-'''
+"""
 Example error handling for unprocessable entity
-'''
+"""
+
+
 @app.errorhandler(422)
 def unprocessable(error):
-    return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
+    return (
+        jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+        422,
+    )
 
-'''
+
+"""
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
@@ -96,15 +109,15 @@ def unprocessable(error):
                     "message": "resource not found"
                     }), 404
 
-'''
+"""
 
-'''
+"""
 @TODO implement error handler for 404
     error handler should conform to general task above 
-'''
+"""
 
 
-'''
+"""
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
-'''
+"""
--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,15 +5,17 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+AUTH0_DOMAIN = "udacity-fsnd.auth0.com"
+ALGORITHMS = ["RS256"]
+API_AUDIENCE = "dev"
 
 ## AuthError Exception
-'''
+"""
 AuthError Exception
 A standardized way to communicate auth failure modes
-'''
+"""
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
@@ -22,18 +24,35 @@ class AuthError(Exception):
 
 ## Auth Header
 
-'''
+"""
 @TODO implement get_token_auth_header() method
     it should attempt to get the header from the request
         it should raise an AuthError if no header is present
     it should attempt to split bearer and the token
         it should raise an AuthError if the header is malformed
     return the token part of the header
-'''
-def get_token_auth_header():
-   raise Exception('Not Implemented')
+"""
 
-'''
+
+def get_token_auth_header():
+
+    if "Authorization" not in request.headers:
+        raise Exception("Not Implemented")
+
+    # get authorization headers
+    auth_header = request.headers["Authorization"]
+
+    # break into parts
+    header_parts = auth_header.split(" ")
+
+    # check to see if header has req parts
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != "bearer":
+        abort(401)
+
+
+"""
 @TODO implement check_permissions(permission, payload) method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
@@ -43,11 +62,14 @@ def get_token_auth_header():
         !!NOTE check your RBAC settings in Auth0
     it should raise an AuthError if the requested permission string is not in the payload permissions array
     return true otherwise
-'''
-def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+"""
 
-'''
+
+def check_permissions(permission, payload):
+    raise Exception("Not Implemented")
+
+
+"""
 @TODO implement verify_decode_jwt(token) method
     @INPUTS
         token: a json web token (string)
@@ -59,11 +81,14 @@ def check_permissions(permission, payload):
     return the decoded payload
 
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
-def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+"""
 
-'''
+
+def verify_decode_jwt(token):
+    raise Exception("Not Implemented")
+
+
+"""
 @TODO implement @requires_auth(permission) decorator method
     @INPUTS
         permission: string permission (i.e. 'post:drink')
@@ -72,8 +97,10 @@ def verify_decode_jwt(token):
     it should use the verify_decode_jwt method to decode the jwt
     it should use the check_permissions method validate claims and check the requested permission
     return the decorator which passes the decoded payload to the decorated method
-'''
-def requires_auth(permission=''):
+"""
+
+
+def requires_auth(permission=""):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
@@ -83,4 +110,6 @@ def requires_auth(permission=''):
             return f(payload, *args, **kwargs)
 
         return wrapper
+
     return requires_auth_decorator
+
