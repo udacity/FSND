e4756511d270d167cade0c7b720936c83e4a507a
Muhammad Hassan <muhamadhhassan@gmail.com>
2020-11-22 22:46:38 +0200

    feat: activate delete venue endpoint

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -126,9 +126,10 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
-
+  venue = Venue.query.get(venue_id)
+  db.session.delete(venue);
+  db.session.commit()
+  
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
   return None
