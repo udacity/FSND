25ff47dc6ebeb315187648aef5461e66799ff54c
Robin Lamb <robinlamb246@gmail.com>
2021-02-21 14:28:12 -0600

    Fix format_datetime method
    Added missing "locale='en'" that causes a crash.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -67,7 +67,7 @@ def format_datetime(value, format='medium'):
       format="EEEE MMMM, d, y 'at' h:mma"
   elif format == 'medium':
       format="EE MM, dd, y h:mma"
-  return babel.dates.format_datetime(date, format)
+  return babel.dates.format_datetime(date, format, locale='en')
 
 app.jinja_env.filters['datetime'] = format_datetime
 
