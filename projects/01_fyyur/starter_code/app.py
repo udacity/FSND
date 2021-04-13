7e666d63e52e30fe7008838ef0aafda132b0dc1a
Brandon Fisher <esnfish@gmail.com>
2021-04-13 17:51:22 -0500

    feature: add flask migrate and initial migration

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -1,5 +1,6 @@
 #----------------------------------------------------------------------------#
 # Imports
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 
 import json
@@ -20,6 +21,8 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
 
