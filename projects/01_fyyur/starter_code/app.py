00cecdafafd0f0131e79dace0493ab28b614d899
jserra02 <joseporiol.serrano@vodafone.com>
2021-12-07 20:07:05 +0100

    update to new schema version

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -37,13 +37,22 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
+    name = db.Column(db.String, nullable = False)
+    city = db.Column(db.String(120), nullable = False)
+    state = db.Column(db.String(120), nullable = False)
+    address = db.Column(db.String(120), nullable = False)
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.String(120), nullable = False)
+    seeking_talent= db.Column(db.Boolean)
+    seeking_description= db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    
+    artists_id = db.relationship("Show", backref="venues")
+    
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>' 
 
     # [ ] TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -51,18 +60,31 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable = False)
+    city = db.Column(db.String(120), nullable = False)
+    state = db.Column(db.String(120), nullable = False)
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.String(120), nullable = False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+    venues_id = db.relationship("Show", backref="artist")
+    
     # [ ] TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # [ ] TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
+#Association Object patern
+class Show(db.Model):
+  __tablename__ = 'Show'
+  
+  artist_id = db.Column(db.ForeignKey('Artist.id'), primary_key=True)
+  venue_id = db.Column(db.ForeignKey('Venue.id'), primary_key=True)
+  start_time = db.Column(db.DateTime)
+  
+  artist = db.relationship("Artist", backref="venues_id")
+  venues = db.relationship("Venue", backref="artists_id")
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
