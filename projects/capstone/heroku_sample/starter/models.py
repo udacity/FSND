769c48505f00e67904ec39bee0131d513a2f37e0
Sudhanshu Kulshrestha <48475411+SudKul@users.noreply.github.com>
2021-12-21 11:49:28 +0530

    Update models.py

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_path = os.environ['DATABASE_URL']
+if database_path.startswith("postgres://"):
+  database_path = database_path.replace("postgres://", "postgresql://", 1)
 
 db = SQLAlchemy()
 
