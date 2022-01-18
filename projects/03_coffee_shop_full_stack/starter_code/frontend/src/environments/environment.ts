16b37b07a9a1e0482d2fae6b4af99dbf8a2fa01e
Shahzod-Ashurmatov <75681220+ShahzodJava@users.noreply.github.com>
2022-01-18 12:04:56 +0500

    finished everything works

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'fsnp-auth.us', // the auth0 domain prefix
+    audience: 'cofee', // the audience set for the auth0 app
+    clientId: 'SCuMk6IIlxVGdipQbXNuF2m8AJoWtY7D', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
