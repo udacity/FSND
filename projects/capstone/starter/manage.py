1cbe6bdbaf539949e88421db140327ec895091b7
GARVITA <garvitasaxena25@gmail.com>
2022-07-11 00:00:16 +0530

    finalcommit

--- projects/capstone/starter/manage.py
+++ projects/capstone/starter/manage.py
@@ -9,6 +9,5 @@ manager = Manager(APP)
 
 manager.add_command('db', MigrateCommand)
 
-
 if __name__ == '__main__':
     manager.run()
--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 export DATABASE_URL="postgresql://postgres:gravity@localhost:5432/postgres"
 echo "setup.sh script executed successfully!"
+
+
+# HEROKU DB URL
+export HEROKU_DATABASE_URL= "postgres://ooymgrtcmwiskr:5bb9428264de42f08050b1689ce8e63e69f993ede53e1daf2d44f9dc706d4e60@ec2-34-233-115-14.compute-1.amazonaws.com:5432/d2ij3q2pu8n2qu"
